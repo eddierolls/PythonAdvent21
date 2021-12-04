3d89fe719efd80144fd07577713fc40561cf49cb
eddierolls <edwardjrolls@gmail.com>
2021-12-04 12:10:21 +0000

    Day 1

--- day1.py
+++ day1.py
@@ -4,4 +4,26 @@ Created on Sat Dec  4 11:45:03 2021
 
 @author: edwar
 """
+from collections import deque
 
+# Part A
+f = open("input/day1.in")
+reading = int(f.readline())
+increases = 0
+for line in f:
+    newReading = int(line)
+    increases += (reading<newReading)
+    reading = newReading
+
+print(increases)
+
+# Part B
+f = open("input/day1.in")
+increases = 0
+queue = deque([int(f.readline()) for _ in range(3)])
+for line in f:
+    oldVal = queue.popleft()
+    queue.append(int(line))
+    increases+=(oldVal<queue[-1])
+print(increases)
+    
